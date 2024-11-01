db81db3820d1ede1ff4fe9dc77e598926167ba4d
hasibprangon <hasibulhossainp09@gmail.com>
2024-11-02 01:29:28 +0600

    using useNavigation to show loader

--- src/Components/Default/Default.jsx
+++ src/Components/Default/Default.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Default = () => {
+    const navigation = useNavigation()
     return (
         <div>
             <Header></Header>
+            {
+                navigation.state === 'loading' ? <p>Loading...</p>:
+
             <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
